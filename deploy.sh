85623eb8a8e3e514637f4af5ee07ec13da5a5209
MartinYY <martin.lk.fun@gmail.com>
2022-03-23 23:00:21 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 # 生成静态文件
 npm run build
 
-# 进入生成的文件夹（ 进入VuePress打包后的文件夹根目录 ）
-cd public
-
 # 如果是发布到自定义域名
 # echo 'www.baidu.com(自定义域名)' > CNAME
 
@@ -12,6 +9,4 @@ git add -A
 git commit -m 'deploy'
 
 
-git push -f git@github.com:MartinYY/codingmartin_blog.git main:gh-pages
-
-rm -rf public
+git push -f git@github.com:MartinYY/codingmartin_blog.git main:master
